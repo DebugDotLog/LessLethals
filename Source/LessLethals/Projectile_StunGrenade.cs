034b1b805fc7b07fd90bc71c3fe24d1096fbc298
DebugDotLog <DebugDotLog@users.noreply.github.com>
2017-01-18 05:45:56 -0500

    Refactor wrt Inheritance
    Instead of copying and modifying the whole Projectile_Explosive class,
    derive from it.
    
    Override only the base Explode() method with original additions. Instead
    of rewriting unchanged/default method calls, call base method.

--- Source/LessLethals/Projectile_StunGrenade.cs
+++ Source/LessLethals/Projectile_StunGrenade.cs
@@ -1,64 +1,16 @@
-﻿using System;
-using Verse;
+﻿using Verse;
 using RimWorld;
 
 namespace LessLethals
 {
-    public class Projectile_StunGrenade : Projectile
+    public class Projectile_StunGrenade : Projectile_Explosive
     {
-        private int ticksToDetonation = 0;
-
-        public override void ExposeData()
-        {
-            base.ExposeData();
-
-            Scribe_Values.LookValue(ref ticksToDetonation, "ticksToDetonation");
-        }
-
-
-        public override void Tick()
-        {
-            base.Tick();
-
-            if (ticksToDetonation > 0)
-            {
-                ticksToDetonation--;
-
-                if (ticksToDetonation <= 0)
-                    Explode();
-            }
-        }
-
-        protected override void Impact(Thing hitThing)
-        {
-            if (def.projectile.explosionDelay == 0)
-            {
-                Explode();
-                return;
-            }
-            else
-            {
-                landed = true;
-                ticksToDetonation = def.projectile.explosionDelay;
-                GenExplosion.NotifyNearbyPawnsOfDangerousExplosive(this, def.projectile.damageDef, launcher.Faction);
-            }
-        }
-
-        protected virtual void Explode()
+        protected override void Explode()
         {
             var map = Map; // before Destroy()!
 
-            Destroy();
+            base.Explode();
 
-            GenExplosion.DoExplosion(Position, map, def.projectile.explosionRadius, def.projectile.damageDef, launcher,
-                explosionSound: def.projectile.soundExplode,
-                projectile: def,
-                source: equipmentDef,
-                postExplosionSpawnThingDef: def.projectile.postExplosionSpawnThingDef,
-                postExplosionSpawnChance: def.projectile.explosionSpawnChance,
-                applyDamageToExplosionCellsNeighbors: false,
-                preExplosionSpawnThingDef: def.projectile.preExplosionSpawnThingDef,
-                preExplosionSpawnChance: def.projectile.explosionSpawnChance);
             //Adds a bright flash to the stun grenade explosion
             MoteMaker.ThrowLightningGlow(base.Position.ToVector3Shifted(), map, 10F);
         }

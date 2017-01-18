543e86cf5974a424b65d7a4153f781e4f95daa1e
DebugDotLog <DebugDotLog@users.noreply.github.com>
2017-01-18 07:05:14 -0500

    Add Summaries and Comments; Build
    Also changed version Iteration to sub-version. (i.e. 0.6.3.0)
    
    Tested!

--- Source/LessLethals/Projectile_StunGrenade.cs
+++ Source/LessLethals/Projectile_StunGrenade.cs
@@ -4,18 +4,30 @@ using UnityEngine;
 
 namespace LessLethals
 {
+    /// <summary>
+    /// Overrides and Executes Explode() from Projectile_Explosive Class adding a custom Mote.
+    /// </summary>
+
+    //  Projectile_Explosive Source: "\RIMWORLD_FOLDER\Source\Verse\Thing\Projectile_Explosive.cs"
     public class Projectile_StunGrenade : Projectile_Explosive
     {
+        /// <summary>
+        /// Overrides Projectile_Explosive.Explode() adding a custom Mote.
+        /// </summary>
+
         protected override void Explode()
         {
+            // Store the custom XML def AS one with the custom <scale> tag
             LL_ThingDef mote_stunGrenade = LL_ThingDefOf.Mote_StunGrenade as LL_ThingDef;
 
-            var map = Map; // before Destroy()!
+            // Save map and (Unity) position before base method destroys the Projectile.
+            var map = Map;
             Vector3 position = ExactPosition;
 
+            // Execute DoExplosion and Destroy from base
             base.Explode();
 
-            //Adds a bright flash to the stun grenade explosion
+            // Generate the custom Mote effect
             MoteMaker.MakeStaticMote(position, map, mote_stunGrenade, mote_stunGrenade.scale);
         }
     }
